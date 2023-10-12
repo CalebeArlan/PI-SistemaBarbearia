ecaae0b7a44ea05d4b203ce8df59c47b317771f5
Artur Dos Santos Carvalho <arturdossantoscarvalho879@gmail.com>
2023-10-12 19:27:00 -0300

    Update Funcoes.cs

--- SistemaBarbearia_PI/SistemaBarbearia_PI/Funcoes.cs
+++ SistemaBarbearia_PI/SistemaBarbearia_PI/Funcoes.cs
@@ -9,7 +9,7 @@ namespace SistemaBarbearia_PI
     public class Funcoes
     {
         public string teste = "teste";
-        public string viuArtur = "ESTÁ VENDO AGORA ABENÇOADESSIMO";
+        public string viuCalebe = "Eu vi em!";
         public static bool VerivicaVazio(Form formulario)
         {
             bool passou = false;
