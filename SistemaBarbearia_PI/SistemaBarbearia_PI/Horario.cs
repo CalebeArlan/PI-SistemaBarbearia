d95fa7e12d2eb48f5c26de274ae094a781cb83f1
Calebe Arlan Souza da Silva <calebearlan@gmail.com>
2023-10-13 18:59:20 -0300

    Desenvolvido a classe que corresponde à tabela Serviços.

---
+++ SistemaBarbearia_PI/SistemaBarbearia_PI/Horario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBarbearia_PI
+{
+	public class Horario
+	{//id 	cod_cliente 	cod_servico 	hora 	data_horario 	
+		public Horario(int idhorario, int codcliente, int codservico, string hora, string datahorario)
+		{
+			this.DataHorario = datahorario;
+			this.IdHorario = idhorario;
+			this.Hora = hora;
+			this.CodCliente = codcliente;
+			this.CodServico = codservico;
+			
+		}
+		public int IdHorario;
+		public int CodCliente;
+		public int CodServico;
+		public string Hora;
+		public string DataHorario;
+	}
+}
