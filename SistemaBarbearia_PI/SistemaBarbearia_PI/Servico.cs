ffdaba5fa19b66592140dda4153a182db1eeae2c
Calebe Arlan Souza da Silva <calebearlan@gmail.com>
2023-10-13 18:54:47 -0300

    Desenvolvida a classe que corresponde à tabela serviços

---
+++ SistemaBarbearia_PI/SistemaBarbearia_PI/Servico.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBarbearia_PI
+{
+	public class Servico
+	{
+		public Servico(int idservico, string nomeservico, double preco, string descricao)
+		{
+			this.IdServico = idservico;
+			this.NomeServico = nomeservico;
+			this.Preco = preco;
+			this.Descricao = descricao;
+		}
+		public int IdServico;
+		public string NomeServico;
+		public double Preco;
+		public string Descricao;
+	}
+}
