dfafec15a2be54d673c664750d7aecb6f4ad61fc
Artur Dos Santos Carvalho <arturdossantoscarvalho879@gmail.com>
2023-10-13 16:02:13 -0300

    Class Cliente Feita, Class Funcionario Feita
    Feito  Class Funcionario e Class Cliente com tudo que tem no banco de dados

---
+++ SistemaBarbearia_PI/SistemaBarbearia_PI/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBarbearia_PI
+{
+    public class Cliente
+    {
+        public Cliente(int idcliente, string nomecliente, string telefonecliente, string emailcliente, int nasccliente, string cpfcliente, string rgcliente)
+        {
+            IdCliente = idcliente;
+            NomeCliente = nomecliente;
+            TelefoneCliente = telefonecliente;
+            EmailCliente = emailcliente;
+            DataNascCliente = nasccliente;
+            CPFCliente = cpfcliente;
+            RGCliente = rgcliente;
+            
+        }
+        public int IdCliente;
+        public string NomeCliente;
+        public string TelefoneCliente;
+        public string EmailCliente;
+        public int DataNascCliente;
+        public string CPFCliente;
+        public string RGCliente;
+
+    }
+}
---
+++ SistemaBarbearia_PI/SistemaBarbearia_PI/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBarbearia_PI
+{
+    public class Funcionario
+    {
+        public Funcionario(int idfuncionario, string nomefuncionario, int nascfuncionario, string cpffuncionario, string rgfuncionario, string enderecofuncionario, int codcargo, string emailfuncionario)
+        {
+            IdFuncionario = idfuncionario;
+            NomeFuncionario = nomefuncionario;
+            DataNascFuncionario = nascfuncionario;
+            CPFFuncionario = cpffuncionario;
+            RGFuncionario = rgfuncionario;
+            EnderecoFuncionario = enderecofuncionario;
+            CodCargo = codcargo;
+            EmailFuncionario = emailfuncionario;
+        }
+        public int IdFuncionario;
+        public string NomeFuncionario;
+        public int DataNascFuncionario;
+        public string CPFFuncionario;
+        public string RGFuncionario;
+        public string EnderecoFuncionario;
+        public int CodCargo;
+        public string EmailFuncionario;
+    }
+}
